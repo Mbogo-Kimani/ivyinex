23eacb9bad44e67c9857335c52e9e20ccb1e9f80
Mbogo Kimani <kimanimbogo1st@gmail.com>
2026-01-24 02:58:50 +0300

    fix: update CORS headers to include additional allowed headers

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin', 'Accept']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin', 'Accept', 'Cache-Control', 'Pragma', 'Expires']
 };
 
 app.use(cors(corsOptions));
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Origin, Accept');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Origin, Accept, Cache-Control, Pragma, Expires');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.sendStatus(200);
 });
